9f1f246d3fdbc0f855bcb9104156f752ec83a0c8
CristiBalazs <necrobis2314@gmail.com>
2019-03-14 21:34:40 +0200

    Add

---
+++ Number1/src/main/java/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] nr = {1, 102, 48, 13,2,0,-4};
+        int max = 0;
+        int min = nr[0];
+        if (nr.length > 0) {
+            for (int i = 0; i <= nr.length - 1; i++) {
+                if (nr[i]<i){
+                    min = nr[i];
+                }
+                if (nr[i]>max)
+                    max = nr[i];
+            }
+        }
+        System.out.println("Your max is " + min);
+        System.out.println("Your max is "+max);
+    }
+}
